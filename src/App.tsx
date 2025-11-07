ba8bdc7397a714778ea1fe3a1692306d49953b05
Juan David Franco <jfrancoh5@gmail.com>
2025-11-07 11:16:09 -0500

    add initial configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from "react"
+
+function App():JSX.Element {
+  return (
+    <main className="bg-main-black h-screen">
+      <h1 className="text-center text-white">Juan David Franco</h1>
+      <p className="mt-4 text-white text-center">Voy a colocar esto a ver que pasa</p>    
+    </main>
+  )
+}
+
+export default App
